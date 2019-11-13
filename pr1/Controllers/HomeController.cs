fb859bb0d8f6c9448f85856329a0f20d25f45286
unknown <zali2008@wp.pl>
2019-11-13 09:01:34 +0100

    Zalozenie pierwszego projektu

---
+++ pr1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using pr1.Extensions;
+using pr1.Models;
+
+namespace pr1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        [Route("185B")]
+        public IActionResult Get158BImage()
+        {
+            var text = Extension.GetDataURL("/185B.png");
+
+            return View(text);
+        }
+    }
+}
